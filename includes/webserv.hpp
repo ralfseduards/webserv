391e4f4f7b4436c64775176f6726dde9a5af9587
user <user@email.com>
2025-03-04 15:33:40 +0100

    a closing bracked was deleted during the merge with main

--- includes/webserv.hpp
+++ includes/webserv.hpp
@@ -114,7 +114,9 @@ int cgi_parse(Client& client);
 #include <string>
 
 template <typename T>
-std::string to_string(const T& value) {
+std::string to_string(const T& value)
+{
     std::ostringstream oss;
     oss << value;
     return oss.str();
+}

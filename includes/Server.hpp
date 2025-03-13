e335744c87668f40e13d854a2df009deb7d9ff24
user <user@email.com>
2025-03-13 16:05:31 +0100

    server compare

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -19,4 +19,10 @@ struct Server
   std::map<std::string, std::string> redirection_table;
   std::map<int, std::string> errorPages;
   bool 			autoindex;
+
+  /* servers must be the same if they have the same name and are on the same socket */
+  bool operator==(const Server& other)
+  {
+    return ((server_name == other.server_name) && (server_socket == other.server_socket));
+  };
 };
